dbc6755373e26e0f75188711e826c30fe6f3f5c4
Rasmus Ros <rasmus.ros@theca.com>
2026-06-08 11:28:00 +0200

    chore: update kbuild to 1.2.6

--- kumulant/build.gradle.kts
+++ kumulant/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-    id("com.eignex.kmp") version "1.2.5"
+    id("com.eignex.kmp") version "1.2.6"
     kotlin("plugin.serialization") version "2.3.20"
 }
 
