3efe905f2e34e699a6eabc1a75e8f5363ece26b2
Rasmus Ros <rasmus.ros@theca.com>
2026-08-08 12:48:33 +0200

    chore: take kbuild 1.2.9

--- kumulant/build.gradle.kts
+++ kumulant/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-    id("com.eignex.kmp") version "1.2.8"
+    id("com.eignex.kmp") version "1.2.9"
     kotlin("plugin.serialization") version "2.4.10"
 }
 
