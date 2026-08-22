ce4a59b9e149e7b37859699cd49d166dcd984c10
Rasmus Ros <rasmus@eignex.com>
2026-08-22 17:56:26 +0200

    chore: bump kbuild to 1.3.2

--- kumulant/build.gradle.kts
+++ kumulant/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import java.util.concurrent.TimeUnit
 
 plugins {
-    id("com.eignex.kmp") version "1.3.1"
+    id("com.eignex.kmp") version "1.3.2"
     kotlin("plugin.serialization") version "2.4.10"
 }
 
