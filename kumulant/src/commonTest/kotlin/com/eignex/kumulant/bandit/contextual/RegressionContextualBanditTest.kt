dd8088d5be106da81139d9eaf5275a6ff3f5d852
Rasmus Ros <rasmus@eignex.com>
2026-08-31 23:00:45 +0200

    style: order contextual test imports

--- kumulant/src/commonTest/kotlin/com/eignex/kumulant/bandit/contextual/RegressionContextualBanditTest.kt
+++ kumulant/src/commonTest/kotlin/com/eignex/kumulant/bandit/contextual/RegressionContextualBanditTest.kt
@@ -1,7 +1,7 @@
 package com.eignex.kumulant.bandit.contextual
 
-import com.eignex.koblas.core.F64DenseVector
 import com.eignex.koblas.Workspace
+import com.eignex.koblas.core.F64DenseVector
 import com.eignex.kumulant.feat
 import com.eignex.kumulant.stat.regression.glm.BayesianRegressionStat
 import com.eignex.kumulant.stat.regression.glm.LinUcb
